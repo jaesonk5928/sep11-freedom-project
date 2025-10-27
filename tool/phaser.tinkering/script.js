5d510bff2b0af6cd91c05b55bd9b68b615300721
Jaeson Kwan <jaesonk@nycstudents.net>
2025-10-27 20:34:44 +0000

    Mon, Oct 27, 2025, 4:34 PM -04:00

--- tool/phaser.tinkering/script.js
+++ tool/phaser.tinkering/script.js
@@ -2,12 +2,14 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-};
+    backgroundColor: 0x000000,
+}
+    // scene: {
+    //     preload: preload,
+    //     create: create,
+    //     update: update,
+    // }
+
 
 var game = new Phaser.Game(config);
 
