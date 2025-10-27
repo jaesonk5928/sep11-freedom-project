7d6cbb2b2c4b5025fcb5cbdea74ac01d92bc0a26
Jaeson Kwan <jaesonk@nycstudents.net>
2025-10-27 23:13:47 +0000

    Mon, Oct 27, 2025, 7:13 PM -04:00

--- tool/phaser.tinkering/Scene2.js
+++ tool/phaser.tinkering/Scene2.js
@@ -1,9 +1,34 @@
-class Scene2 extends Phaser.Scene{
-  constructor(){
+// class Scene2 extends Phaser.Scene{
+//   constructor(){
+//     super("playGame");
+//   }
+
+//   create(){
+//     this.add.text(20, 20, "Loading...", {font: "25px Arial", fill: "yellow"});
+//   }
+// }
+class Scene2 extends Phaser.Scene {
+  constructor() {
     super("playGame");
   }
 
-  create(){
-    this.add.text(20, 20, "Loading...", {font: "25px Arial", fill: "yellow"});
+  create() {
+    this.background = this.add.image(0, 0, "background");
+    this.background.setOrigin(0, 0);
+
+    this.ship1 = this.add.image(config.width / 2 - 50, config.height / 2, "ship");
+    this.ship2 = this.add.image(config.width / 2, config.height / 2, "ship2");
+    this.ship3 = this.add.image(config.width / 2 + 50, config.height / 2, "ship3");
+
+    this.add.text(20, 20, "Playing game", {
+      font: "25px Arial",
+      fill: "yellow"
+    });
+
+    // this.ship1.setScale(5);
+    // this.ship1.setOrigin(0.5);
+    // this.ship1.flipY = true;
+    // this.ship1.angle = 90;
+
   }
 }
