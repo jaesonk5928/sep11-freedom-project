f309630fe7b4f045382d96af9e9e0f1aa41c8cd2
Jaeson Kwan <jaesonk@nycstudents.net>
2025-10-29 01:49:38 +0000

    Tue, Oct 28, 2025, 9:49 PM -04:00

--- tool/phaser.tinkering/script.js
+++ tool/phaser.tinkering/script.js
@@ -28,14 +28,6 @@
 // }
 
 var config = {
-  type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-    default: 'arcade',
-      arcade: {
-   gravity: { y: 300 },
-      debug: false
 
   width: 250,
   height: 400,
