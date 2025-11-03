68b8dda812b944ce97fa2f0fb5ab724b83e0f6df
Jaeson Kwan <jaesonk@nycstudents.net>
2025-11-03 17:02:03 +0000

    Mon, Nov 3, 2025, 12:02 PM -05:00

--- tool/phaser.tinkering/script.js
+++ tool/phaser.tinkering/script.js
@@ -28,13 +28,11 @@
 // }
 
 var config = {
-
-  width: 1850,
-  height: 900,
+  width: 256,
+  height: 272,
   backgroundColor: 0x000000,
-  scene: [Scene1, Scene2]
-
+  scene: [Scene1, Scene2],
+  pixelArt: true
 }
-
 
 var game = new Phaser.Game(config);
