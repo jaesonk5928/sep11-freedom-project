2cb11a5a06a4ecb9ed68de987a71da9c8cf57c9d
Jaeson Kwan <jaesonk@nycstudents.net>
2025-11-17 18:22:19 +0000

    Mon, Nov 17, 2025, 1:22 PM -05:00

--- tool/phaser.tinkering/Scene1.js
+++ tool/phaser.tinkering/Scene1.js
@@ -28,19 +28,19 @@ class Scene1 extends Phaser.Scene {
     // this.load.image("ship2", "assets/images/ship2.png");
     // this.load.image("ship3", "assets/images/ship3.png");
     // to
-    this.load.spritesheet("ship", "assets/spritesheets/ship.png",{
+    this.load.spritesheet("ship", "assets/spritesheets/ship.mhtml",{
       frameWidth: 16,
       frameHeight: 16
     });
-    this.load.spritesheet("ship2", "assets/spritesheets/ship2.png",{
+    this.load.spritesheet("ship2", "assets/spritesheets/ship2.mhtml",{
       frameWidth: 32,
       frameHeight: 16
     });
-    this.load.spritesheet("ship3", "assets/spritesheets/ship3.png",{
+    this.load.spritesheet("ship3", "assets/spritesheets/ship3.mhtml",{
       frameWidth: 32,
       frameHeight: 32
     });
-    this.load.spritesheet("explosion", "assets/spritesheets/explosion.png",{
+    this.load.spritesheet("explosion", "assets/spritesheets/explosion.mhtml",{
       frameWidth: 16,
       frameHeight: 16
     });
