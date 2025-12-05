1e8cf8ecc006151efaebfd15f5e481161fb6e54f
Jaeson Kwan <jaesonk@nycstudents.net>
2025-12-05 11:35:09 -0500

    added LL5

--- tool/phaser.tinkering/Scene1.js
+++ tool/phaser.tinkering/Scene1.js
@@ -1,21 +1,3 @@
-// class Scene1 extends Phaser.Scene {
-//   constructor() {
-//     super("bootGame");
-//   }
-
-//   preload(){
-//     this.load.image("background", "assets/images/background.png");
-//     this.load.image("ship", "assets/images/ship.png");
-//     this.load.image("ship2", "assets/images/ship2.png");
-//     this.load.image("ship3", "assets/images/ship3.png");
-//   }
-
-//   create() {
-//     this.add.text(20, 20, "Loading game...");
-//     this.scene.start("playGame");
-//   }
-// }
-
 class Scene1 extends Phaser.Scene {
   constructor() {
     super("bootGame");
@@ -23,27 +5,9 @@ class Scene1 extends Phaser.Scene {
 
   preload(){
     this.load.image("background", "assets/images/background.png");
-    // 0 chnage the path to spritesheets
-    // this.load.image("ship", "assets/images/ship.png");
-    // this.load.image("ship2", "assets/images/ship2.png");
-    // this.load.image("ship3", "assets/images/ship3.png");
-    // to
-    this.load.spritesheet("ship", "assets/spritesheets/ship.mhtml",{
-      frameWidth: 16,
-      frameHeight: 16
-    });
-    this.load.spritesheet("ship2", "assets/spritesheets/ship2.mhtml",{
-      frameWidth: 32,
-      frameHeight: 16
-    });
-    this.load.spritesheet("ship3", "assets/spritesheets/ship3.mhtml",{
-      frameWidth: 32,
-      frameHeight: 32
-    });
-    this.load.spritesheet("explosion", "assets/spritesheets/explosion.mhtml",{
-      frameWidth: 16,
-      frameHeight: 16
-    });
+    this.load.image("ship", "assets/images/ship.png");
+    this.load.image("ship2", "assets/images/ship2.png");
+    this.load.image("ship3", "assets/images/ship3.png");
   }
 
   create() {
--- tool/phaser.tinkering/script.js
+++ tool/phaser.tinkering/script.js
@@ -1,6 +1,6 @@
 var config = {
-  width: 272,
-  height: 256,
+  width: 256,
+  height: 272,
   backgroundColor: 0x000000,
   scene: [Scene1, Scene2],
   pixelArt: true
