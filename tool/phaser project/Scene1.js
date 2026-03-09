1e27598d7de96ab0a25761ef2f6bcc37a1980378
Jaeson Kwan <jaesonk@nycstudents.net>
2026-03-09 18:03:01 +0000

    Mon, Mar 9, 2026, 2:03 PM -04:00

--- tool/phaser project/Scene1.js
+++ tool/phaser project/Scene1.js
@@ -16,45 +16,5 @@ preload(){
   }
 }
 
-class Scene2 extends Phaser.Scene{
-  constructor(){
-    super("playGame");
-}
-
-  create(){
-    // this.add.text(20, 20, "Game", {font: "25px Arial", fill: "yellow"});
-    const bigBg = this.add.image(400, 350, "background");
-    const player = this.add.image(400, 350, "player");
-    player.setScale(0.04)
-    bigBg.setScale(2.15);
-  }
-}
-
-  let charX = 100;
-  let charY = 150;
-const speed = 5;
-
-const keys = {};
-
-  player.onload = () => {
-    requestAnimationFrame(update);
-  };
-
-  window.addEventListener('keydown', (e) => {
-    keys[e.key] = true;
-  });
-
-  window.addEventListener('keyup', (e) => {
-    keys[e.key] = false;
-  });
-
-  function update() {
-    if (keys['ArrowUp']) charY -= speed;
-    if (keys['ArrowDown']) charY += speed;
-    if (keys['ArrowLeft']) charX -= speed;
-    if (keys['ArrowRight']) charX += speed;
-
-}
-
 
 
