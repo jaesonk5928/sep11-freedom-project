bc74557ab95e1b98ae55e70ac5c7e5ce857d9a97
Jaeson Kwan <jaesonk@nycstudents.net>
2026-03-09 13:38:30 -0400

    added stuff

--- tool/phaser project/Scene1.js
+++ tool/phaser project/Scene1.js
@@ -6,7 +6,7 @@ class Scene1 extends Phaser.Scene {
 
 preload(){
     this.load.image("background", "assets/images/darkBg.png");
-    this.load.image("player", "assets/images/player.png");
+    this.load.image("player", "assets/images/stickman.png");
   }
 
   create() {
@@ -15,4 +15,46 @@ preload(){
     this.add.image(400, 300, 'background');
   }
 }
+
+class Scene2 extends Phaser.Scene{
+  constructor(){
+    super("playGame");
+}
+
+  create(){
+    // this.add.text(20, 20, "Game", {font: "25px Arial", fill: "yellow"});
+    const bigBg = this.add.image(400, 350, "background");
+    const player = this.add.image(400, 350, "player");
+    player.setScale(0.04)
+    bigBg.setScale(2.15);
+  }
+}
+
+  let charX = 100;
+  let charY = 150;
+const speed = 5;
+
+const keys = {};
+
+  player.onload = () => {
+    requestAnimationFrame(update);
+  };
+
+  window.addEventListener('keydown', (e) => {
+    keys[e.key] = true;
+  });
+
+  window.addEventListener('keyup', (e) => {
+    keys[e.key] = false;
+  });
+
+  function update() {
+    if (keys['ArrowUp']) charY -= speed;
+    if (keys['ArrowDown']) charY += speed;
+    if (keys['ArrowLeft']) charX -= speed;
+    if (keys['ArrowRight']) charX += speed;
+
+}
+
+
 
