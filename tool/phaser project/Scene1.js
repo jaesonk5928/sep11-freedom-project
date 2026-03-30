23b6ac0320f3f028c365cf783f4b404e2eb43d08
Jaeson Kwan <jaesonk@nycstudents.net>
2026-03-30 17:22:10 +0000

    Mon, Mar 30, 2026, 1:22 PM -04:00

--- tool/phaser project/Scene1.js
+++ tool/phaser project/Scene1.js
@@ -4,7 +4,7 @@ class Scene1 extends Phaser.Scene {
   }
 
   preload(){
-    this.load.image("background", "assets/images/back.jpg");
+    this.load.image('background', 'assets/images/back.png');
   }
 
   create() {
