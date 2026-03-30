815f89c2ef70d67651594339800953e6801f3c33
Jaeson Kwan <jaesonk@nycstudents.net>
2026-03-30 17:32:19 +0000

    Mon, Mar 30, 2026, 1:32 PM -04:00

--- tool/phaser project/Scene1.js
+++ tool/phaser project/Scene1.js
@@ -5,12 +5,15 @@ class Scene1 extends Phaser.Scene {
 
   preload(){
     this.load.image('background', 'assets/images/back.png');
-    this.load.image('powerUp', 'assets/images/power.png')
+    this.load.image('powerUp', 'assets/images/power.png');
+    this.load.image('error', 'assets/images/danger.png');
   }
 
   create() {
     this.add.image(600, 300, 'background');
     this.add.image(100, 200, 'powerUp');
+    this.add.image(800, 300, 'error');
+
 
   }
 }
--- tool/phaser project/game.js
+++ tool/phaser project/game.js
@@ -1,8 +1,21 @@
 var config = {
-  width: 1200,
-  height: 800,
+  width: 800,
+  height: 600,
   backgroundColor: 0x000000,
   scene: [Scene1, Scene2]
+  type: Phaser.AUTO,
+  physics: {
+    default: 'arcade',
+    arcade: {
+        gravity: { y: 300 },
+        debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
 };
 
 var game = new Phaser.Game(config);
