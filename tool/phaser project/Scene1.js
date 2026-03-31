176771c2ac264456cf2a51d3ac174f130ab8ca4a
Jaeson Kwan <jaesonk@nycstudents.net>
2026-03-31 17:26:48 +0000

    Tue, Mar 31, 2026, 1:26 PM -04:00

--- tool/phaser project/Scene1.js
+++ tool/phaser project/Scene1.js
@@ -5,13 +5,13 @@ class Scene1 extends Phaser.Scene {
 
   preload(){
     this.load.image('background', 'assets/images/back.png');
-    this.load.image('powerUp', 'assets/images/power.png');
+    this.load.image('ground', 'assets/images/ground.png');
     this.load.image('error', 'assets/images/danger.png');
   }
 
   create() {
     const back = this.add.image(800, 300, 'background');
-    // const powering = this.add.image(100, 200, 'powerUp');
+    const ground = this.add.image(100, 200, 'ground');
     // this.add.image(800, 300, 'error');
 
     back.scale = 2.8;
