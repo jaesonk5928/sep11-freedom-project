59bc8fd398bfcece1559926aa1fcead35a12991f
Jaeson Kwan <jaesonk@nycstudents.net>
2026-03-31 17:39:17 +0000

    Tue, Mar 31, 2026, 1:39 PM -04:00

--- tool/phaser project/game.js
+++ tool/phaser project/game.js
@@ -2,7 +2,14 @@ var config = {
   width: 1600,
   height: 1600,
   backgroundColor: 0x000000,
-  scene: [Scene1]
+  physics: {
+    default: 'arcade',
+      arcade: {
+        gravity: { y: 300 },
+          debug: false
+      }
+  },
+      scene: [Scene1]
 };
 
 var game = new Phaser.Game(config);
