17319bdc95f76c2ffdd4281821d8b8f941d60995
Jaeson Kwan <jaesonk@nycstudents.net>
2026-04-04 14:52:16 +0000

    Sat, Apr 4, 2026, 10:52 AM -04:00

--- tool/phaser-project/Scene1.js
+++ tool/phaser-project/Scene1.js
@@ -1,88 +1,3 @@
-// class Scene1 extends Phaser.Scene {
-//   constructor() {
-//     super("bootGame");
-//   }
-
-//   preload(){
-//     this.load.image('platform', 'assets/images/platform.png');
-//     this.load.spritesheet('dude', 'assets/spritesheets/dude.png',{
-//          frameWidth: 32,
-//          frameHeight: 48
-//     });
-//     this.load.image('background', 'assets/images/back.png');
-//   }
-
-//   create() {
-//     var player = this.physics.add.sprite(200, 375, 'dude');
-//     player.setBounce(0.3);
-//     player.setCollideWorldBounds(true);
-//     player.body.setGravityY(100);
-//     this.physics.world.createDebugGraphic();
-//     this.cursors = this.input.keyboard.createCursorKeys();
-
-
-//     var jumpPad;
-//     const back = this.add.image(800, 300, 'background');
-//     const pad = this.physics.add.staticImage(150, 670, 'platform').setDisplaySize(300, 50).refreshBody(); // bottom
-
-//     jumpPad = this.physics.add.staticGroup();
-
-//     jumpPad.create(700, 100, 'platform').setDisplaySize(300, 50).refreshBody(); // mid
-//     jumpPad.create(500, 300, 'platform').setDisplaySize(300, 50).refreshBody(); // top
-
-//     this.physics.add.collider(player, pad);
-//     player.setDepth(1);
-
-//     back.scale = 2.8;
-//     this.anims.create({
-//         key: 'left',
-//         frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-//         frameRate: 10,
-//         repeat: -1
-//     });
-
-//     this.anims.create({
-//         key: 'turn',
-//         frames: [ { key: 'dude', frame: 4 } ],
-//         frameRate: 20
-//     });
-
-//     this.anims.create({
-//         key: 'right',
-//         frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-//         frameRate: 10,
-//         repeat: -1
-//     });
-
-//   }
-//   update () {
-//     // if (cursors.left.isDown)
-//     // {
-//     //   player.setVelocityX(-160);
-
-//     //   player.anims.play('left', true);
-//     // }
-//     // else if (cursors.right.isDown)
-//     // {
-//     //   player.setVelocityX(160);
-
-//     //   player.anims.play('right', true);
-//     // }
-//     // else
-//     // {
-//     //   player.setVelocityX(0);
-
-//     //   player.anims.play('turn');
-//     // }
-
-//     // if (cursors.up.isDown) // && player.body.touching.down)
-//     // {
-//     //   player.setVelocityY(-330);
-//     // }
-
-// }
-// }
-
 class Scene1 extends Phaser.Scene {
   constructor() {
     super("bootGame");
@@ -104,10 +19,12 @@ class Scene1 extends Phaser.Scene {
     back.setDepth(-1);  // Set depth to make sure the background stays behind other elements
 
     // Create platforms
-    const pad = this.physics.add.staticImage(150, 670, 'platform').setDisplaySize(300, 40).refreshBody(); // bottom
+    const pad = this.physics.add.staticImage(150, 670, 'platform').setDisplaySize(175, 50).refreshBody(); // bottom
     const jumpPad = this.physics.add.staticGroup();
-    jumpPad.create(700, 100, 'platform').setDisplaySize(300, 40).refreshBody(); // mid
-    jumpPad.create(500, 300, 'platform').setDisplaySize(300, 40).refreshBody(); // top
+    jumpPad.create(700, 100, 'platform').setDisplaySize(175, 50).refreshBody(); // mid
+    jumpPad.create(500, 300, 'platform').setDisplaySize(175, 50).refreshBody(); // top
+    jumpPad.create(300, 600, 'platform').setDisplaySize(175, 50).refreshBody(); // mid
+
 
     // Create player sprite
     var player = this.physics.add.sprite(300, 375, 'dude');
@@ -154,9 +71,8 @@ class Scene1 extends Phaser.Scene {
   }
 
   update() {
-    // Handle player movement
+    // player movement
     const player = this.player;
-
     if (this.cursors.left.isDown) {
       player.setVelocityX(-150); // Move left
       player.anims.play('left', true); // Plays left animation
@@ -167,9 +83,8 @@ class Scene1 extends Phaser.Scene {
       player.setVelocityX(0); // Stop moving X-axis
       player.anims.play('turn', true); // Play idle animation
     }
-
     if (this.cursors.up.isDown && player.body.touching.down) {
-      player.setVelocityY(-630); // Jumps
+      player.setVelocityY(-330); // Jumps
     }
   }
 }
