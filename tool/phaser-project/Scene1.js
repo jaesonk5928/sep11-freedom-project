e1bfec74b8c60a2cc257ee738fea4777d65be6e7
Jaeson Kwan <jaesonk@nycstudents.net>
2026-04-04 15:06:55 +0000

    Sat, Apr 4, 2026, 11:06 AM -04:00

--- tool/phaser-project/Scene1.js
+++ tool/phaser-project/Scene1.js
@@ -1,3 +1,95 @@
+// class Scene1 extends Phaser.Scene {
+//   constructor() {
+//     super("bootGame");
+//   }
+
+//   preload() {
+//     this.load.image('background', 'assets/images/back.png');
+//     this.load.image('platform', 'assets/images/platform.png');
+//     this.load.image('powerUp', 'assets/images/power.png');
+//     this.load.spritesheet('dude', 'assets/spritesheets/dude.png', {
+//       frameWidth: 32,
+//       frameHeight: 48
+//     });
+//   }
+
+//   create() {
+//     // Background first, scale it and ensure it stays behind
+//     const back = this.add.image(800, 300, 'background');
+//     back.scale = 2.8;
+//     back.setDepth(-1);  // Set depth to make sure the background stays behind other elements
+
+//     // Create platforms
+//     const pad = this.physics.add.staticImage(150, 670, 'platform').setDisplaySize(175, 60).refreshBody(); // bottom
+//     const jumpPad = this.physics.add.staticGroup();
+//     jumpPad.create(700, 100, 'platform').setDisplaySize(175, 60).refreshBody(); // mid
+//     jumpPad.create(500, 300, 'platform').setDisplaySize(175, 60).refreshBody(); // top
+//     jumpPad.create(600, 600, 'platform').setDisplaySize(175, 60).refreshBody(); // bottom mid
+
+
+//     // Create player sprite
+//     var player = this.physics.add.sprite(150, 575, 'dude');
+//     player.setBounce(0.1);
+//     player.setCollideWorldBounds(true);
+//     player.body.setGravityY(200);
+
+//     // Set up player animation
+//     this.anims.create({
+//       key: 'left',
+//       frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+//       frameRate: 10,
+//       repeat: -1
+//     });
+
+//     this.anims.create({
+//       key: 'turn',
+//       frames: [{ key: 'dude', frame: 4 }],
+//       frameRate: 20
+//     });
+
+//     this.anims.create({
+//       key: 'right',
+//       frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+//       frameRate: 10,
+//       repeat: -1
+//     });
+
+//     // Make sure player sprite is on top of other elements
+//     player.setDepth(1);  // Ensure player is drawn on top of background
+
+//     // Set collision for player and platforms
+//     this.physics.add.collider(player, pad);
+//     this.physics.add.collider(player, jumpPad);
+
+//     // Create debug graphics for collision
+//     this.physics.world.createDebugGraphic();
+
+//     // Set up cursor keys for player movement
+//     this.cursors = this.input.keyboard.createCursorKeys();
+
+//     // Store the player object as part of the scene for later use in update
+//     this.player = player;
+//   }
+
+//   update() {
+//     // player movement
+//     const player = this.player;
+//     if (this.cursors.left.isDown) {
+//       player.setVelocityX(-150); // Move left
+//       player.anims.play('left', true); // Plays left animation
+//     } else if (this.cursors.right.isDown) {
+//       player.setVelocityX(150); // Move right
+//       player.anims.play('right', true); // Plays right animation
+//     } else {
+//       player.setVelocityX(0); // Stop moving X-axis
+//       player.anims.play('turn', true); // Play idle animation
+//     }
+//     if (this.cursors.up.isDown && player.body.touching.down) {
+//       player.setVelocityY(-330); // Jumps
+//     }
+//   }
+// }
+
 class Scene1 extends Phaser.Scene {
   constructor() {
     super("bootGame");
@@ -25,7 +117,6 @@ class Scene1 extends Phaser.Scene {
     jumpPad.create(700, 100, 'platform').setDisplaySize(175, 60).refreshBody(); // mid
     jumpPad.create(500, 300, 'platform').setDisplaySize(175, 60).refreshBody(); // top
     jumpPad.create(600, 600, 'platform').setDisplaySize(175, 60).refreshBody(); // bottom mid
-
 
     // Create player sprite
     var player = this.physics.add.sprite(150, 575, 'dude');
@@ -67,6 +158,13 @@ class Scene1 extends Phaser.Scene {
     // Set up cursor keys for player movement
     this.cursors = this.input.keyboard.createCursorKeys();
 
+    // Create power-up object
+    this.powerUp = this.physics.add.image(700, 75, 'powerUp').setScale(0.1);
+    this.powerUp.setCollideWorldBounds(true); // Keep the power-up inside the world bounds
+
+    // Add overlap between player and power-up
+    this.physics.add.overlap(player, this.powerUp, this.collectPowerUp, null, this);
+
     // Store the player object as part of the scene for later use in update
     this.player = player;
   }
@@ -88,5 +186,24 @@ class Scene1 extends Phaser.Scene {
       player.setVelocityY(-330); // Jumps
     }
   }
-}
 
+  // Power-up collection function
+  collectPowerUp(player, powerUp, pad) {
+    // Remove the power-up from the game world
+    powerUp.setVisible(false);
+    powerUp.setActive(false);
+
+    // You can add more effects here (like boosting player speed)
+    console.log("Power-up collected!");
+
+    // Example: Boost the player's speed temporarily
+    player.setVelocityX(300); // Temporarily increase speed to 300
+
+    // Optionally, re-enable the power-up after a short delay
+    this.time.delayedCall(2000, () => {
+      powerUp.setVisible(true);
+      powerUp.setActive(true);
+      powerUp.setPosition(400, 500); // Reset position (or change to a new location)
+    });
+  }
+}
