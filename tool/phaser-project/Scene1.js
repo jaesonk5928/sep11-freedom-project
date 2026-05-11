106542a721fb1380d9f449cac5fdc0d11a781d8c
Jaeson Kwan <jaesonk@nycstudents.net>
2026-05-11 18:05:55 +0000

    Mon, May 11, 2026, 2:05 PM -04:00

--- tool/phaser-project/Scene1.js
+++ tool/phaser-project/Scene1.js
@@ -1,3 +1,241 @@
+// // class Scene1 extends Phaser.Scene {
+// //   constructor() {
+// //     super("bootGame");
+// //     this.powerUps = [];
+// //   }
+
+// //   // loads in all images/spritesheets in assests
+// //   preload() {
+// //     this.load.image('background', 'assets/images/back.png');
+// //     this.load.image('platform', 'assets/images/platform.png');
+// //     this.load.image('powerUp', 'assets/images/power.png');
+// //     this.load.image('alert', 'assets/images/fire.png');
+// //     this.load.spritesheet('dude', 'assets/spritesheets/dude.png', {
+// //       frameWidth: 32,
+// //       frameHeight: 48
+// //     });
+// //   }
+
+// //   create() {
+// //     // game functions (specific time frames)
+// //     this.deadNow = false;
+// //     this.winnerS = false;
+// //     this.winScore = 1000;
+
+// //     // sets the background
+// //     const back = this.add.image(800, 300, 'background');
+// //     back.setScale(3);
+// //     back.setDepth(-1);
+
+// //     let randomX = Phaser.Math.Between(200, 300);
+// //     let randomY = Phaser.Math.Between(300, 400);
+
+// //     // counts the score
+// //     this.score = 0;
+// //     this.scoreText = this.add.text(16, 16, 'Score: 0', {
+// //       fontSize: '32px',
+// //       fill: '#000'
+// //     });
+
+// //     // platform initiation
+// //     const pad = this.physics.add.staticImage(175, 670, 'platform')
+// //       .setDisplaySize(125, 35)
+// //       .refreshBody();
+
+// //     // draws all the platforms
+// //     const jumpPad = this.physics.add.staticGroup();
+// //     jumpPad.create(825, 450, 'platform').setDisplaySize(125, 35).refreshBody();
+// //     jumpPad.create(550, 280, 'platform').setDisplaySize(125, 35).refreshBody();
+// //     jumpPad.create(500, 600, 'platform').setDisplaySize(125, 35).refreshBody();
+// //     jumpPad.create(randomX, randomY, 'platform').setDisplaySize(125, 35).refreshBody();
+// //     jumpPad.create(1000, randomY, 'platform').setDisplaySize(125, 35).refreshBody();
+
+// //     // adds functions to player (physics & collision)
+// //     this.player = this.physics.add.sprite(180, 575, 'dude');
+// //     this.player.setBounce(0.2);
+// //     this.player.setCollideWorldBounds(true);
+// //     this.player.body.setGravityY(200);
+
+// //     // the created sprite animations
+// //     this.anims.create({
+// //       key: 'left',
+// //       frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+// //       frameRate: 10,
+// //       repeat: -1
+// //     });
+// //     this.anims.create({
+// //       key: 'turn',
+// //       frames: [{ key: 'dude', frame: 4 }],
+// //       frameRate: 20
+// //     });
+// //     this.anims.create({
+// //       key: 'right',
+// //       frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+// //       frameRate: 10,
+// //       repeat: -1
+// //     });
+
+// //     // Press r to restart
+// //     this.input.keyboard.on('keydown-R', () => {
+// //       this.scene.restart();
+// //     });
+
+// //     // adds collision for both platform
+// //     this.physics.add.collider(this.player, pad);
+// //     this.physics.add.collider(this.player, jumpPad);
+
+// //     // adds powerUps
+// //     this.powerUpGroup = this.physics.add.group({
+// //       key: 'powerUp',
+// //       repeat: 100,
+// //       setXY: { x: 300, y: 800, stepX: 200 }
+// //     });
+
+// //     // groups the powerUp function
+// //     this.powerUpGroup.getChildren().forEach(powerUp => {
+// //       powerUp.setPosition(
+// //         Phaser.Math.Between(200, 1000),
+// //         Phaser.Math.Between(100, 800)
+// //       );
+// //       powerUp.setBounce(0.2);
+// //       powerUp.setCollideWorldBounds(true);
+// //       powerUp.body.setGravityY(200);
+// //       powerUp.setScale(0.05);
+// //     });
+
+// //     this.physics.add.collider(this.powerUpGroup, jumpPad);
+// //     this.physics.add.collider(this.powerUpGroup, pad);
+
+// //     this.physics.add.overlap(
+// //       this.player,
+// //       this.powerUpGroup,
+// //       this.collectPowerUp,
+// //       null,
+// //       this
+// //     );
+
+// //     // the fire group
+// //     this.fires = this.physics.add.group();
+// //     for (let i = 0; i < 3; i++) {
+// //       const fire = this.fires.create(
+// //         Phaser.Math.Between(100, 1000),
+// //         Phaser.Math.Between(0, 800),
+// //         'alert'
+// //       );
+
+// //       fire.setBounce(1);
+// //       fire.setCollideWorldBounds(true);
+// //       fire.setVelocity(
+// //         Phaser.Math.Between(-200, 600),
+// //         Phaser.Math.Between(20, 300)
+// //       );
+// //       fire.setScale(0.065);
+// //     }
+
+// //     this.physics.add.collider(this.fires, pad);
+// //     this.physics.add.collider(this.fires, jumpPad);
+
+// //     this.physics.add.collider(
+// //       this.player,
+// //       this.fires,
+// //       this.hitFire,
+// //       null,
+// //       this
+// //     );
+
+// //     this.cursors = this.input.keyboard.createCursorKeys();
+// //   }
+
+// //   // cursors for arrow key to move the character
+// //   update() {
+// //     if (this.isDead || this.hasWon) return;
+
+// //     const player = this.player;
+
+// //     if (this.cursors.left.isDown) {
+// //       player.setVelocityX(-150);
+// //       player.anims.play('left', true);
+// //     } else if (this.cursors.right.isDown) {
+// //       player.setVelocityX(150);
+// //       player.anims.play('right', true);
+// //     } else {
+// //       player.setVelocityX(0);
+// //       player.anims.play('turn');
+// //     }
+// //     if (this.cursors.up.isDown && player.body.touching.down) {
+// //       player.setVelocityY(-460);
+// //     }
+// //   }
+
+// //   collectPowerUp(player, powerUp) {
+// //     powerUp.disableBody(true, true);
+
+// //     this.score += 25;
+// //     this.scoreText.setText('Score: ' + this.score + ' (Get 1000 points to win)');
+
+// //     // function that checks if you win
+// //     if (this.score >= this.winScore && !this.winnerS) {
+// //       this.triggerWin();
+// //       return;
+// //     }
+
+// //     // respawn
+// //     this.time.delayedCall(5000, () => {
+// //       powerUp.enableBody(
+// //         true,
+// //         Phaser.Math.Between(50, 800),
+// //         Phaser.Math.Between(300, 800),
+// //         true,
+// //         true
+// //       );
+
+// //       powerUp.setVelocity(0, 0);
+// //       powerUp.setBounce(0.2);
+// //       powerUp.setGravityY(200);
+// //     });
+// //   }
+
+// //   // THIS section is about win/death screen
+// //   hitFire(player, fire) {
+// //     if (this.deadNow || this.hasWon) return;
+// //     this.deadNow = true;
+// //     this.physics.pause();
+
+// //     player.setTint(0xff0000);
+// //     player.anims.play('turn');
+
+// //     var death = this.add.rectangle(400, 300, 800, 600, 0x000000, 1);
+// //     death.setScale(2);
+
+// //     this.add.text(600, 300, 'GAME OVER!', {
+// //       fontSize: '64px',
+// //       fill: '#ff0000'
+// //     }).setOrigin(0.5);
+// //     this.add.text(600, 350, 'Press R to Restart', {
+// //       fontSize: '28px',
+// //       fill: '#ffffff'
+// //     }).setOrigin(0.5);
+// //   }
+
+// //   triggerWin() {
+// //     this.hasWon = true;
+// //     this.physics.pause();
+// //     this.player.setVelocity(0, 0);
+// //     this.player.anims.play('turn');
+
+// //     var win = this.add.rectangle(400, 300, 800, 600, 0x000000, 1);
+// //     win.setScale(2);
+
+// //     this.add.text(600, 300, "YOU WIN!", {
+// //       fontSize: '64px',
+// //       fill: '#00ff00'
+// //     }).setOrigin(0.5);
+// //     this.add.text(600, 350, 'Press R to Restart', {
+// //       fontSize: '28px',
+// //       fill: '#ffffff'
+// //     }).setOrigin(0.5);
+// //   }
+// // }
 // class Scene1 extends Phaser.Scene {
 //   constructor() {
 //     super("bootGame");
@@ -85,22 +323,14 @@
 //     this.physics.add.collider(this.player, jumpPad);
 
 //     // adds powerUps
-//     this.powerUpGroup = this.physics.add.group({
-//       key: 'powerUp',
-//       repeat: 100,
-//       setXY: { x: 300, y: 800, stepX: 200 }
-//     });
+//     this.powerUpGroup = this.physics.add.group();
 
-//     // groups the powerUp function
-//     this.powerUpGroup.getChildren().forEach(powerUp => {
-//       powerUp.setPosition(
-//         Phaser.Math.Between(200, 1000),
-//         Phaser.Math.Between(100, 800)
-//       );
-//       powerUp.setBounce(0.2);
-//       powerUp.setCollideWorldBounds(true);
-//       powerUp.body.setGravityY(200);
-//       powerUp.setScale(0.05);
+//     // spawn powerUps with delay
+//     this.time.addEvent({
+//       delay: 100,
+//       callback: this.spawnPowerUp,
+//       callbackScope: this,
+//       loop: true
 //     });
 
 //     this.physics.add.collider(this.powerUpGroup, jumpPad);
@@ -144,6 +374,21 @@
 //     );
 
 //     this.cursors = this.input.keyboard.createCursorKeys();
+//   }
+
+//   spawnPowerUp() {
+//     if (this.powerUpGroup.countActive(true) >= 10) return;
+
+//     const powerUp = this.powerUpGroup.create(
+//       Phaser.Math.Between(200, 1000),
+//       Phaser.Math.Between(100, 400),
+//       'powerUp'
+//     );
+
+//     powerUp.setBounce(0.2);
+//     powerUp.setCollideWorldBounds(true);
+//     powerUp.body.setGravityY(200);
+//     powerUp.setScale(0.05);
 //   }
 
 //   // cursors for arrow key to move the character
@@ -327,7 +572,7 @@ class Scene1 extends Phaser.Scene {
 
     // spawn powerUps with delay
     this.time.addEvent({
-      delay: 300,
+      delay: 3000,
       callback: this.spawnPowerUp,
       callbackScope: this,
       loop: true
@@ -377,8 +622,6 @@ class Scene1 extends Phaser.Scene {
   }
 
   spawnPowerUp() {
-    if (this.powerUpGroup.countActive(true) >= 10) return;
-
     const powerUp = this.powerUpGroup.create(
       Phaser.Math.Between(200, 1000),
       Phaser.Math.Between(100, 400),
